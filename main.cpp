fb815f4b836d5febaf5a62a26aafb97d6a35b2f5
qqq142 <nekrasova.valeriia@mail.ru>
2026-03-23 21:10:17 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <Windows.h>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int counter{};
+    int num{};
+
+    std::cout << "¬ведите целое число или число '0', чтобы закончить:" << std::endl;
+    std::cin >> num;
+    while (num != 0) {
+        counter += num;
+        std::cout << "¬ведите целое число или число '0', чтобы закончить:" << std::endl;
+        std::cin >> num;
+    }
+    std::cout << "—умма: " << counter;
+
+
+    return 0;
+}
